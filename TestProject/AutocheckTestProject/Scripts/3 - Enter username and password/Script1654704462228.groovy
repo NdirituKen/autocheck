36822dbf2e0f668249e99902135351d5e0307b99
NdirituKen <kennethndiritu@Kenneths-MacBook-Pro.local>
2022-06-08 22:06:30 +0300

    Login and homepage scripts

---
+++ TestProject/AutocheckTestProject/Scripts/3 - Enter username and password/Script1654704462228.groovy
@@ -0,0 +1,32 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+
+//Click the username field
+WebUI.click(findTestObject('1 - Sign in Object repo/input_Email Address_email'))
+
+//Enter the username
+WebUI.setText(findTestObject('1 - Sign in Object repo/input_Email Address_email'), 'memee7583@gmail.com')
+
+//click on the passowrd field
+WebUI.click(findTestObject('1 - Sign in Object repo/input_Password_password'))
+
+//enter the password
+WebUI.setEncryptedText(findTestObject('1 - Sign in Object repo/input_Password_password'), 'gYGUm/a+j4iO226NSkR9ZQ==')
+
